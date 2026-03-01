4a112e3aa5d93ee747a9e9ddf487222b0e709fd5
NavyaMaragoni <navyamaragoni@gmail.com>
2026-03-01 18:56:08 +0530

    second commit from feature branch2

--- first.java
+++ first.java
@@ -1,5 +1,5 @@
 public class first{
     public static void main(String [] args){
-        System.out.println("hello world from branch feTURE branch2!!!");
+        System.out.println("hello world  2from branch feTURE branch2!!!");
     }
 }
